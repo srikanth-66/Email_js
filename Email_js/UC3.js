ac6690b22484084f6fae4655ba020d4a41d30764
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-08-31 21:57:13 +0530

    u3 commit

---
+++ Email_js/UC3.js
@@ -0,0 +1,17 @@
+class Email{
+    checkPatternEmail(data){
+
+        var pattern=RegExp("^[0-9A-Za-z.]+@bridgelabz.co");
+
+        if(pattern.test(data)){
+            console.log("valid email");
+        }else{
+            console.log("invalid email");    
+        }
+
+    }
+}
+
+const checkPattern=new Email();
+checkPattern.checkPatternEmail("abc.xyz@bridgelabz.co.in");
+checkPattern.checkPatternEmail("abc@co.in"); 
