d859d2b46f37573e7ff2e6dc63f131cfedac200b
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-08-31 21:16:16 +0530

    uc4 commit

---
+++ postalJs/UC4.js
@@ -0,0 +1,16 @@
+class PinCode{
+    checkPatternNumber(data){
+
+        var pattern=RegExp("[0-9\s]+{6}");
+
+        if(pattern.test(data)){
+            console.log("valid code");
+        }else{
+            console.log("invalid code");    
+        }
+
+    }
+}
+
+const checkPattern=new PinCode();
+checkPattern.checkPatternNumber("431001"); 
